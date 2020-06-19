72471d803055cff31357bf454ab9b2052a06568d
dimaematveev <dimaematveev@gmail.com>
2020-06-19 13:47:51 +0300

    6 ready

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -10,6 +10,41 @@ namespace Task6
     {
         static void Main(string[] args)
         {
+            int N = 7;
+            int[] h = new int[]
+            {
+                1,3,2,9,2,5,4
+            };
+
+            int max = h[0];
+            int count = 1;
+            for (int i = 1; i < N; i++)
+            {
+                if (h[i]==h[i-1])
+                {
+                    count++;
+                }
+                else
+                {
+                    int maxtemp= h[i - 1] * count;
+                    if (maxtemp>max)
+                    {
+                        max = maxtemp;
+                    }
+                    count = 1;
+                }
+            }
+            {
+                int maxtemp = h[N-1] * count;
+                if (maxtemp > max)
+                {
+                    max = maxtemp;
+                }
+            }
+
+
+            Console.WriteLine(max);
+            Console.ReadKey();
         }
     }
 }
